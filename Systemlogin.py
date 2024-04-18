720e25c75967915bc9053243b8e374b0f02ccd12
b0ssbota <148467827+b0ssbota@users.noreply.github.com>
2024-04-18 11:26:54 +0100

    asowfk

---
+++ Systemlogin.py
@@ -0,0 +1,8 @@
+with open("Userloginsystem.csv") as csv:
+    lines = csv.readlines()
+    for i in range(1, len(lines)):
+        parsedline = lines[i].split(",")
+        users = [
+            parsedline[0],
+            parsedline[1],
+        ]
